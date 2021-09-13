b2067dd96adb5ba2181dd5b441775606587538bb
Rodrigo Lorandi <rodrigo_lorandi@hotmail.com>
2021-09-13 18:44:32 -0300

    Create vetores

---
+++ Vetores/src/Ocupation.java
@@ -0,0 +1,46 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Ocupation {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+
+        Scanner scan = new Scanner(System.in);
+
+        int n = scan.nextInt();
+
+        Room[] room = new Room[10];
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("Rent #" + (i + 1) + ":");
+            scan.nextLine();
+
+            System.out.print("Name:");
+            String name = scan.nextLine();
+
+            System.out.print("Email: ");
+            String email = scan.nextLine();
+
+            System.out.print("Room: ");
+            int number = scan.nextInt();
+            room[number - 1] = new Room(number, name, email);
+            System.out.println();
+        }
+
+        System.out.println("Busy roons: ");
+
+        for (int i = 0; i < room.length; i++) {
+
+            if (room[i] != null) {
+                System.out.println(
+                        room[i].getNumber()
+                                + ": "
+                                + room[i].getNome()
+                                + ", "
+                                + room[i].getEmail());
+            }
+        }
+
+        scan.close();
+    }
+}
---
+++ Vetores/src/Room.java
@@ -0,0 +1,35 @@
+public class Room {
+    private int number;
+    private String nome;
+    private String email;
+
+    public Room(int number, String nome, String email) {
+        this.number = number;
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
