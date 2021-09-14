1701af816954f4d2887cf0279ec07f8d83f3a07c
Rodrigo Lorandi <rodrigo_lorandi@hotmail.com>
2021-09-14 19:46:16 -0300

    refactor ListaFuncionarios

--- ListaFuncionarios/src/aplication/Program.java
+++ ListaFuncionarios/src/aplication/Program.java
@@ -4,24 +4,25 @@ import entities.Employee;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class Program {
 
     public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
         List<Employee> listEmployee = new ArrayList<>();
 
         Scanner scan = new Scanner(System.in);
-
-        Employee employee;
 
         System.out.print("How many employees will be registered? ");
 
         int number = scan.nextInt();
 
         for (int i = 0; i < number; i++) {
-            System.out.println("Employee #" + (i + 1));
+            System.out.println("Employee #" + (i + 1) + ":");
 
             System.out.print("Id: ");
             int id = scan.nextInt();
@@ -35,10 +36,7 @@ public class Program {
 
             listEmployee.add(new Employee(id, name, salary));
             System.out.println();
-
         }
-
-
 
         System.out.print("Enter the employee id that will have the salary increase: ");
         int idIncreaseSalary = scan.nextInt();
