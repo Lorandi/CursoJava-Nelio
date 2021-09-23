9e0f7c5986c1a87cb70bd116e425cf43b66281af
Rodrigo Lorandi <rodrigo_lorandi@hotmail.com>
2021-09-23 16:15:04 -0300

    create exercicio exception

---
+++ 15-TratamentoDeExcecoes/exercicioDeFixacao/src/application/Program.java
@@ -0,0 +1,53 @@
+package application;
+
+import model.entities.Account;
+import model.exceptions.DomainException;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter account data");
+        System.out.print("Number: ");
+        int number = sc.nextInt();
+
+        System.out.print("Holder: ");
+        sc.nextLine();
+        String holder = sc.nextLine();
+
+        System.out.print("Inicial balance: ");
+        double balance = sc.nextDouble();
+
+
+        System.out.print("Withdraw limit: ");
+        double withdrawLimit = sc.nextDouble();
+
+        Account c = new Account(number, holder, balance, withdrawLimit);
+
+        System.out.println();
+
+        System.out.print("Enter amount for withdraw: ");
+
+        double withdraw = sc.nextDouble();
+
+        try {
+            c.withdraw(withdraw);
+
+        } catch (DomainException e) {
+            System.out.println("Withdraw error: " + e.getMessage());
+        } catch (RuntimeException e) {
+            System.out.println("Unexpected error");
+        }
+        sc.close();
+
+    }
+
+
+}
